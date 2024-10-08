bc4e34f05144717b85e597aa667f0fdf343f65f0
Kendall Martinez <kendallmartinezcarvajal4@gmail.com>
2024-10-08 16:38:33 -0600

    Renombre del codigo de Arduino, e implementacion de todos los pines
    Se necesita llegar a un concenso de que palabras se van a usar y que se va a mandar para poder avanzar mas en el desarrollo del back end del hardware

--- ArdCom/SetupHardware/SetupHardware.ino
+++ ArdCom/SetupHardware/SetupHardware.ino
@@ -1,7 +1,21 @@
-int ledPin = 2;
+int ledPin1 = 2;
+int ledPin2 = 3;
+int ledPin3 = 4;
+int ledPin4 = 5;
+int ledPin5 = 6;
+int ledPin6 = 7;
+int ledPin7 = 8;
+int ledPin8 = 9;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // Configura el pin 2 como salida
+  pinMode(ledPin1, OUTPUT);  // Configura el pin 2 como salida
+  pinMode(ledPin2, OUTPUT);  // Configura el pin 3 como salida
+  pinMode(ledPin3, OUTPUT);  // Configura el pin 4 como salida
+  pinMode(ledPin4, OUTPUT);  // Configura el pin 5 como salida
+  pinMode(ledPin5, OUTPUT);  // Configura el pin 6 como salida
+  pinMode(ledPin6, OUTPUT);  // Configura el pin 7 como salida
+  pinMode(ledPin7, OUTPUT);  // Configura el pin 8 como salida
+  pinMode(ledPin8, OUTPUT);  // Configura el pin 9 como salida
   Serial.begin(9600);       // Inicializa la comunicación serial a 9600 baudios
 }
 
